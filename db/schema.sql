22609e0928fbf09dcf8966f7fa0ebf63c23fd689
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-17 18:54:58 -0800

    latest code

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,22 @@
-DROP DATABASE IF EXISTS books_db;
-CREATE DATABASE books_db;
+ DROP DATABASE IF EXISTS employees_db;
+ CREATE DATABASE employees_db;
+ USE employees_db;
 
-USE books_db;
-
-CREATE TABLE favorite_books (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  section INT NOT NULL,
-  book_name VARCHAR(30) NOT NULL,
-  in_stock BOOLEAN,
-  quantity INT NOT NULL
-);
+ CREATE TABLE sections (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   section_name VARCHAR(30) NOT NULL
+ );
+ CREATE TABLE positions ( 
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  position_name VARCHAR(30) NOT NULL,
+  position_salary INT NOT NULL,
+  position_section_id INT
+ );
+ CREATE TABLE employees ( 
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   employee_first_name VARCHAR(30) NOT NULL,
+   employee_last_name VARCHAR(30) NOT NULL,
+   employee_position_id INT NOT NULL,
+   employee_section_id INT,
+   employee_manager VARCHAR(300)
+ );
--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,15 @@
-INSERT INTO favorite_books (book_name, section, in_stock, quantity)
-VALUES ("The Great Gatsby", 1, true, 11),
-       ("Huckleberry Finn", 1, true, 4),
-       ("100 Years of Solitude", 3, false, 0),
-       ("Things Fall Apart", 4 , false, 0 ),
-       ("Crime and Punishment", 1, true, 15),
-       ("Moby Dick", 2, true, 11),
-       ("Decameron", 5, false, 0),
-       ("The Wind and the Willows", 2, true, 11),
-       ("Ulysses", 3, true, 8),
-       ("Madame Bovary", 4 , true, 9 ),
-       ("Catcher in the Rye", 1, true, 23),
-       ("Catch-12", 5, true, 18),
-       ("Hamlet", 5, true, 14);
+INSERT INTO sections (section_name)
+VALUES ("eComm"),
+       ("Media"),
+       ("Production"),
+       ("Accounting");
+INSERT INTO postions (position_name, position_salary, position_department_id)
+VALUES ("eComm", 30000, 1),
+       ("Media", 34000, 2),
+       ("Production", 37000, 3),
+       ("Accounting", 37000, 4);
+INSERT INTO employees (employee_first_name, employee_last_name, employee_position_id, employee_section_id, employee_manager)
+VALUES ("Juan", "the Productionist", 1,1, "Leon"),
+       ("Melly", "The Accountant", 2,2, "Albert"),
+       ("Andrew", "Ernie", 3,3, "Albert"),
+       ("Ernie", "Garcia", 4,4, "Leon");
