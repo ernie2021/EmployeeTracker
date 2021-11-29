5f0d751b3d87334474911ebc386a35b2dbc5e4b8
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-28 22:08:42 -0800

    updated server.js file

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 const { query } = require("./config/connection");
-//connection
 const db = require("./config/connection")
-//indexMenu
-const indexOption = require("./options/index")
+const indexOption = require("./options/index");
+
+
+const questions = [
+    {
+        type: "list",
+        name: "indexChoice",
+        message: "What would you like to do at the moment?",
+        choices: ["View All Sections", "View All Positions", "View All Employees", "Add A Section", "Add A Position", "Add An Employee", "Update An Employee Position"]
+    }
+];
+
+function indexOptions () {
+    inquirer.prompt(questions).then((data) => {
+        switch (data.indexChoice) {
+            case "View All Sections":
+                allSections();
+                break;
+            case "View All Positions":
+                allPositions();
+                break;
+            case "View All Employees":
+                seeAllEmployees();
+                break;
+            case "Add A Department":
+                sectionOptions();
+                break;
+            case "Add A Role":
+                addPositionOptions();
+                break;
+            case "Add An Employee":
+                employeeOptions();
+                break;
+            case "Update An Employee Role":
+                updatePositionOptions();
+                break;
+        
+            default:
+                console.log("error")
+                break;
+        }
+    })
+}
 
 function init () {
     indexOption()
 }
 
 init();
+
+module.exports = indexOptions
